cbf00ad0a0326ff60e259f9b5eced702fb1c6f6d
feoff <feoff3@gmail.com>
2015-09-08 21:50:51 +0300

    updated grub

--- gcimagebundle/gcimagebundlelib/grub.py
+++ gcimagebundle/gcimagebundlelib/grub.py
@@ -114,9 +114,13 @@ def InstallGrub(mount_point , partition_dev):
     else:
         logging.error("!!!ERROR: cannot find a partition \ disk to install GRUB")
         raise OSError("Cannot find partition to install GRUB")
+    
+    devmap = "mount_point+/device.map"
+    with open(devmap , "w") as f:
+        f.write("(hd0)\t"+diskpath+"\n(hd0,1)\t"+partition_path)
     # install grub2 there
     # NOTE: GRUB2 settings and kernel\initrd images should be imported from the local disk!
-    RunCommand(["grub-install", str(diskpath), "--root-directory=" + mount_point , "--recheck"])
+    RunCommand(["grub-install", str(diskpath), "--root-directory=" + mount_point , "--recheck" , "--grub-mkdevicemap="+devmap])
       
     uuid = RunCommand(["blkid", "-s", "UUID", "-o" , "value", partition_dev])
     uuid = str(uuid).strip()
